d6f4df318014c775bb1f4a011071756f230520a5
Till Wegmueller <toasterson@gmail.com>
2023-09-07 20:01:37 +0200

    Remove old tenantd code
    Signed-off-by: Till Wegmueller <toasterson@gmail.com>

--- tenantd/src/database/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod principal;
-pub mod schema;
-pub mod tenant;
-
-use diesel::r2d2::{ConnectionManager, Pool};
-use diesel::PgConnection;
-use std::sync::Arc;
-
-pub type PGPool = Arc<Pool<ConnectionManager<PgConnection>>>;
